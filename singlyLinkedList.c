018c759c52a4395785977a42acadfaa2e2025961
Sai Sreekar <sai.sreekhar@gmail.com>
2022-04-03 19:49:39 +0530

    added insertion at head of singlylinkedlist

--- singlyLinkedList.c
+++ singlyLinkedList.c
@@ -5,9 +5,52 @@
 #include <math.h>
 #include <ctype.h>
 
+typedef struct Node
+{
+    int data;
+    struct Node *next;
+} node;
+
+node *head = NULL;
+
+void insertAtHead(int data)
+{
+    node *link = (node *)malloc(sizeof(node));
+
+    if (link == NULL)
+    {
+        printf("Memory Allocation Failed");
+        return;
+    }
+    
+    link->data = data;
+    link->next = head;
+    head = link;
+
+    return;
+}
+
+void displaySinglyLinkedList()
+{
+    node *curNode = head;
+    int pos = 1;
+
+    while (curNode != NULL)
+    {
+        printf("The element as position %d is %d\n", pos, curNode->data);
+        curNode = curNode->next;
+        pos++;
+    }
+
+    return;
+}
 
 int main()
 {
+    insertAtHead(100);
+    insertAtHead(1000);
     
+    displaySinglyLinkedList();
+
     return 0;
 }
