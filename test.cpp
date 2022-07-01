b9a0ff9a0d957d5a1879e30f5eb149f8f0666ccc
sai-sreekhar <sai.sreekhar@gmail.com>
2022-07-01 13:10:45 +0530

    Double Hashing Commit

--- test.cpp
+++ test.cpp
@@ -1,17 +1,57 @@
 #include <iostream>
+#include <string>
+#include <cstring>
+#include <new>
+#include <cstdbool>
+#include <cstdlib>
+#include <cmath>
+#include <cctype>
+#include <cstdarg>
+
+#define DEBUG
+
+#ifdef DEBUG
+#define TRACE(msg, ...) printTrace(__LINE__, __FILE__, msg, __VA_ARGS__)
+#else
+#define TRACE(msg, ...)
+#endif
+
 using namespace std;
-int func(int i)
+
+void printTrace(int line, const char *fileName, const char *msg, ...)
 {
-    if (i == 1)
-    {
-        return i;
-    }
+    va_list args;
+    char buffer[1024+2] = { 0 };
+
+    snprintf(buffer, sizeof(buffer), "%s(%d) : " , fileName, line);
+    int stringLength = strnlen(buffer,1024);
+    int remainingBufferSize = 1024 - stringLength;
+    va_start(args, msg);
+    vsnprintf(buffer+stringLength,remainingBufferSize, msg, args);
+    va_end(args);
+    cout << buffer << flush;
 }
+
 int main()
 {
-    int i = 5;
-    int j = 1;
-    cout << func(i) << endl;
-    cout << func(j) << endl;
+    string ans = "";
+    string presentStr = "";
+    for(int i = 1; i<3; i++)
+    {
+        if (i == 1)
+        {
+            ans.append("()");
+            presentStr = "()";
+        }       
+        else
+        {
+            ans.append("("+presentStr+")");
+        }
+    }
+    
+    string str1 = "(";
+    str1.append(ans);
+    ans.append(")");
+    cout << ans << endl;
     return 0;
 }
