bfd29c589660a65f0654672f647d27145a3d4e09
sai-sreekhar <sai.sreekhar@gmail.com>
2022-07-01 18:01:53 +0530

    insert func updated

--- Hashing/stringHashing.cpp
+++ Hashing/stringHashing.cpp
@@ -30,6 +30,7 @@ void printTrace(int line, const char *fileName, const char *msg, ...)
     int stringLength = strnlen(buffer, 1024);
     int remainingBufferSize = 1024 - stringLength;
     va_start(args, msg);
+
     vsnprintf(buffer + stringLength, remainingBufferSize, msg, args);
     va_end(args);
     cout << buffer << flush;
@@ -37,172 +38,179 @@ void printTrace(int line, const char *fileName, const char *msg, ...)
 
 typedef struct Student
 {
-    string* rollNo;
-    string* name;
-    Student* next;
-}student;
+    string *rollNo;
+    string *name;
+    Student *next;
+} student;
 
 class stringHash
 {
+    private:
 
-    int keysPresent;
-    student* hashTable[MAX_SIZE];
+        int keysPresent;
+        student *hashTable[MAX_SIZE];
 
-    int computeHash(string s)
-    {
-        int hash = 0;
-        for (int i = 0; i < s.length(); i++)
+        int computeHash(string s)
         {
-            hash += s[i];
-        }
-        return (hash % MAX_SIZE);
-    }
+            int hash = 0;
 
-    bool isFull()
-    {
-        return (MAX_SIZE == keysPresent);
-    }
-
-public:
-    stringHash()
-    {
-        keysPresent = 0;
-        memset(hashTable,0,sizeof(hashTable));
-    }
-
-    void insert(string rollNo,string name)
-    {
-        if (isFull())
-        {
-            cout << ("ERROR : Hash Table Full\n");
-            return;
-        }
-
-        int hashVal = computeHash(rollNo);
-        
-        student* newStudent = new student;
-        newStudent->name = new string(name);
-        newStudent->rollNo = new string(rollNo);
-        newStudent->next = nullptr;
-
-        if (hashTable[hashVal] == nullptr)
-        {
-            hashTable[hashVal] = newStudent;
-            keysPresent += 1;
-            return;
-        }
-
-        student* pvsStudent = hashTable[hashVal];
-        student* presentStudent =  hashTable[hashVal]->next;
-        while (presentStudent != NULL)
-        {
-            presentStudent = presentStudent->next;
-        }
-
-        presentStudent = newStudent;
-        pvsStudent->next = presentStudent;
-        keysPresent += 1;
-
-        return;
-    }
-
-    void erase(string s)
-    {
-        bool found = false;
-        int hashVal = computeHash(s);
-
-        student* pvsStudent = nullptr;
-        student* presentStudent =  hashTable[hashVal];
-        while (presentStudent != nullptr)
-        {
-            if (*(presentStudent->rollNo) == s)
+            for (int i = 0; i < s.length(); i++)
             {
-                if (pvsStudent != nullptr)
-                {
-                    pvsStudent->next = presentStudent->next;
-                }
-                if (pvsStudent == nullptr)
-                {
-                    if (presentStudent->next != nullptr)
-                    {
-                        hashTable[hashVal] = presentStudent->next;
-                    }
-                    else
-                    {
-                        hashTable[hashVal] = NULL;
-                    }
-                }
-                
-                delete  presentStudent->name;
-                delete presentStudent->rollNo;
-                delete presentStudent;
+                hash += s[i];
+            }
+            return (hash % MAX_SIZE);
+        }
 
-                found = true;
-                keysPresent--;
+        bool isFull()
+        {
+            return (MAX_SIZE == keysPresent);
+        }
+
+    public:
+
+        stringHash()
+        {
+            keysPresent = 0;
+            memset(hashTable, 0, sizeof(hashTable));
+        }
+
+        void insert(string rollNo, string name)
+        {
+            if (isFull())
+            {
+                cout << ("ERROR : Hash Table Full\n");
                 return;
             }
-            else
+
+            int hashVal = computeHash(rollNo);
+
+            student *newStudent = new student;
+            newStudent->name = new string(name);
+            newStudent->rollNo = new string(rollNo);
+            newStudent->next = nullptr;
+
+            if (hashTable[hashVal] == nullptr)
+            {
+                hashTable[hashVal] = newStudent;
+                keysPresent += 1;
+                return;
+            }
+
+            student *pvsStudent = hashTable[hashVal];
+            student *presentStudent = hashTable[hashVal]->next;
+
+            while (presentStudent != NULL)
             {
                 pvsStudent = presentStudent;
                 presentStudent = presentStudent->next;
             }
+
+            presentStudent = newStudent;
+            pvsStudent->next = presentStudent;
+            keysPresent += 1;
+
+            return;
         }
 
-        if (found == false)
+        void erase(string s)
         {
-            cout << "Roll Number not found" << endl;
-        }
-        
-    }
+            bool found = false;
+            int hashVal = computeHash(s);
 
-    bool search(string s)
-    {
-        int hashVal = computeHash(s);
-        
-        if (hashTable[hashVal] == nullptr)
-        {
-            return false;
-        }
-        else
-        {
-            student* presentStudent =  hashTable[hashVal];
+            student *pvsStudent = nullptr;
+            student *presentStudent = hashTable[hashVal];
+
             while (presentStudent != nullptr)
             {
                 if (*(presentStudent->rollNo) == s)
                 {
-                    return true;
+                    if (pvsStudent != nullptr)
+                    {
+                        pvsStudent->next = presentStudent->next;
+                    }
+
+                    if (pvsStudent == nullptr)
+                    {
+                        if (presentStudent->next != nullptr)
+                        {
+                            hashTable[hashVal] = presentStudent->next;
+                        }
+                        else
+                        {
+                            hashTable[hashVal] = NULL;
+                        }
+                    }
+
+                    delete presentStudent->name;
+                    delete presentStudent->rollNo;
+                    delete presentStudent;
+
+                    found = true;
+                    keysPresent--;
+                    return;
                 }
                 else
                 {
+                    pvsStudent = presentStudent;
+                    presentStudent = presentStudent->next;
+                }
+            }
+
+            if (found == false)
+            {
+                cout << "Roll Number not found" << endl;
+            }
+        }
+
+        bool search(string s)
+        {
+            int hashVal = computeHash(s);
+
+            if (hashTable[hashVal] == nullptr)
+            {
+                return false;
+            }
+            else
+            {
+                student *presentStudent = hashTable[hashVal];
+                while (presentStudent != nullptr)
+                {
+                    if (*(presentStudent->rollNo) == s)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        presentStudent = presentStudent->next;
+                    }
+                }
+            }
+            return false;
+        }
+
+        void print()
+        {
+            for (int i = 0; i < MAX_SIZE; i++)
+            {
+                student *presentStudent = hashTable[i];
+                while (presentStudent != nullptr)
+                {
+                    cout << "Roll Number: " << *(presentStudent->rollNo) << " Name: " << *(presentStudent->name) << endl;
                     presentStudent = presentStudent->next;
                 }
             }
         }
-        return false;
-    }
-    
-    void print()
-    {
-        for (int i = 0; i < MAX_SIZE; i++)
-        {
-            student* presentStudent =  hashTable[i];
-            while (presentStudent != nullptr)
-            {
-                cout << "Roll Number: " << *(presentStudent->rollNo) << " Name: " << *(presentStudent->name) << endl;
-                presentStudent = presentStudent->next;
-            }
-        }
-    }
 };
 
 int main()
 {
 
-    stringHash myHash; 
-    myHash.insert("21BKT0162","SaiSreekar");
-    myHash.insert("21BDS0162","Sai");
-    myHash.insert("21BKT2610","Sai2");
+    stringHash myHash;
+    myHash.insert("21BKT0162", "SaiSreekar");
+    myHash.insert("21BDS0162", "Sai1");
+    myHash.insert("21BKT2610", "Sai2");
+    myHash.insert("21BKT6210","Sai3");
     cout << myHash.search("21BKT2610") << endl;
-    myHash.erase("21BKT0162");
+    myHash.erase("21BKT2610");
     myHash.print();
 }
